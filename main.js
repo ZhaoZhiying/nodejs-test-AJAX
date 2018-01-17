26f2ea491ee1bab401a1b61a468d351fe0e87bc4
ZhaoZhiYing <31733268+ZhaoZhiYing@users.noreply.github.com>
2018-01-17 17:33:09 +0800

    JS 操作请求与响应

--- main.js
+++ main.js
@@ -1,24 +1,23 @@
 myButton.addEventListener('click', (e)=>{
     let request = new XMLHttpRequest()
-    request.open('GET', 'https://www.baidu.com/') // 配置 request 
-    request.send()
+    request.open('POST', '/xxx') //设置请求第一部分
+    request.setRequestHeader('zz', '18') //设置请求第二部分
+    request.setRequestHeader('Content-Type', 'x-www-form-urlencoded')
+    request.send('我是第四部分') //设置请求第四部分
     request.onreadystatechange = () =>{ 
         if(request.readyState === 4){
             console.log('请求响应完毕')
+            console.log(request.status) //获取响应第一部分 
+            console.log(request.statusText) //获取响应第一部分 
             if(request.status >= 200 && request.status < 300){
                 console.log('请求成功')
-                console.log(request.responseText)
-                console.log(typeof request.responseType)
-
+                console.log(request.getAllResponseHeaders())//获取响应第二部分
+                console.log(request.getResponseHeader('Content-Type'))//获取响应第二部分
+                console.log(request.responseText)//获取响应第四部分
                 let string =  request.responseText
                 // 把符合 JSON 语法的字符串
                 // 转换成 JS 对应的值
                 let object = window.JSON.parse(string)//JSON.parse 是浏览器提供的
-
-                console.log(typeof object)
-                console.log(object)
-                console.log('object.note')
-                console.log(object.note)
 
             }else if(request.status >= 400){
                 console.log('请求失败')
