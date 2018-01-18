bcb038f1e386c67d5d1d8762182c28f0006ad911
ZhaoZhiYing <31733268+ZhaoZhiYing@users.noreply.github.com>
2018-01-18 12:15:20 +0800

    window.jQuery.ajax

--- main.js
+++ main.js
@@ -1,27 +1,32 @@
-myButton.addEventListener('click', (e)=>{
-    let request = new XMLHttpRequest()
-    request.open('POST', '/xxx') //设置请求第一部分
-    request.setRequestHeader('zz', '18') //设置请求第二部分
-    request.setRequestHeader('Content-Type', 'x-www-form-urlencoded')
-    request.send('我是第四部分') //设置请求第四部分
-    request.onreadystatechange = () =>{ 
-        if(request.readyState === 4){
-            console.log('请求响应完毕')
-            console.log(request.status) //获取响应第一部分 
-            console.log(request.statusText) //获取响应第一部分 
-            if(request.status >= 200 && request.status < 300){
-                console.log('请求成功')
-                console.log(request.getAllResponseHeaders())//获取响应第二部分
-                console.log(request.getResponseHeader('Content-Type'))//获取响应第二部分
-                console.log(request.responseText)//获取响应第四部分
-                let string =  request.responseText
-                // 把符合 JSON 语法的字符串
-                // 转换成 JS 对应的值
-                let object = window.JSON.parse(string)//JSON.parse 是浏览器提供的
+window.jQuery = function(nodeOrSelector){
+    let nodes = {}
+    nodes.addClass = function(){}
+    nodes.html = function(){}
+    return nodes
+}
 
+window.jQuery.ajax = function(url, method, body, successFn, failFn){
+    let request = new XMLHttpRequest()
+    request.open(method, url) 
+    request.onreadystatechange = () =>{ 
+        if(request.readyState === 4){     
+            if(request.status >= 200 && request.status < 300){
+                successFn.call(undefined, request.responseText)
             }else if(request.status >= 400){
-                console.log('请求失败')
+                failFn.call(undefined, request)
             }
         }
     }
+    request.send(body) 
+}
+window.$ = window.jQuery
+
+myButton.addEventListener('click', (e)=>{
+    window.jQuery.ajax(
+        '/xxx', 
+        'post', 
+        'a=1&b=2', 
+        (responseText)=>{console.log(1)}, 
+        (request)=>{console.log(2)}
+    )
 })
